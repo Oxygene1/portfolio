052c922021f82f3b8f9ab3ac0d74fef603872782
Abdulganiyu Ibrahim <97368641+Oxygene1@users.noreply.github.com>
2023-09-14 06:54:10 +0100

    chat bot added

--- src/App.tsx
+++ src/App.tsx
@@ -599,19 +599,15 @@ function App() {
         </Typography>
       </Box>
 
-      <Box>
+      {/* <Box>
         <PopupWidget
           url="https://calendly.com/abdulganiyutechng"
-          /*
-           * react-calendly uses React's Portal feature (https://reactjs.org/docs/portals.html) to render the popup modal. As a result, you'll need to
-           * specify the rootElement property to ensure the modal is inserted into the correct domNode.
-           */
           rootElement={document.getElementById("root")!}
           text="Schedule a meeting with Ibrahim!"
           textColor="#ffffff"
           color="gray"
         />
-      </Box>
+      </Box> */}
     </>
   );
 }

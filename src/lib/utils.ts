79fb11b0dde9b872b1f00c529cbc377e6e479f6d
Sunday Oruwhone <sundayoruwhone@gmail.com>
2025-06-21 19:32:25 +0100

    removed unused imports

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,5 +1,3 @@
-import { ClassValue, clsx } from "clsx";
-import { twMerge } from "tailwind-merge";
 
 export function cn(...args: (string | false | undefined | null)[]): string {
   return args.filter(Boolean).join(" ");
